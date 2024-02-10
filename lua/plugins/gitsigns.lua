f644db1df1c60cb97f1fcfbda4af5679960fa76f
Aydin BAKIR <aydintb@users.noreply.github.com>
2024-02-10 21:26:45 +0300

    🌀 neovim config - git key binding

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -2,6 +2,8 @@ return {
 	"lewis6991/gitsigns.nvim",
 	config = function()
 		require("gitsigns").setup()
-		vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
-	end,
+ 		vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+ 		vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
+  end,
+
 }
